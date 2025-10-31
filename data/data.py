a5e2761f87c053c0fa53d69ffad5678cca253e0c
Hossam Aldeen <hossam.aldeen.theking2022yahoo@gmail.com>
2025-10-31 23:15:18 +0200

    Add data.py with static book data

---
+++ data/data.py
@@ -0,0 +1,13 @@
+# data.py
+# يحتوي على كل ملفات file_id أو البيانات الثابتة
+
+BOOKS = {
+    "1st": {  # الصف الأول الثانوي
+        "arabic": "BQADAgADwQEAAtYy6UqZbJ8LJ7QGnAI",
+        "math": "BQADAgADxwEAAtYy6Uo-lPvlXfW2AI",
+    },
+    "2nd": {  # الصف الثاني الثانوي
+        "science": "BQADAgADxgEAAtYy6UrkF9d8R9A0AI",
+        "history": "BQADAgADxAEAAtYy6Urx7I8OozQ2AI",
+    }
+}
